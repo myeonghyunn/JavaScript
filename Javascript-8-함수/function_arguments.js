92ba9e5a40a7e6b25d886f2c8c72a2c92ead3603
Myeong Hyun Park <qwef106@gmail.com>
2021-11-08 14:21:28 +0900

    Add files via upload

---
+++ Javascript-8-함수/function_arguments.js
@@ -0,0 +1,5 @@
+function get_arguments(arg1, arg2) {
+    return arg1 / arg2;
+  }
+  console.log(get_arguments(30,10));
+  console.log(get_arguments(100,10));
---
+++ Javascript-8-함수/function_return.js
@@ -0,0 +1,12 @@
+function get_me () {
+    return 'PARK MYEONG HYUN';
+  }
+  console.log(get_me()); //PARK MYEONG HYUN
+  
+  function greetings () {
+    return 'Hello';
+    return 'World';
+    return 'PARK';
+    return 'MYEONGHYUN';
+  }
+  console.log(greetings()); //Hello
