7ee2511324422dba79e1d8f48b4e6c2c0e5e982f
Myeong Hyun Park <qwef106@gmail.com>
2021-11-14 16:30:20 +0900

    Add files via upload

---
+++ JavaScript-9-배열/var_greetings.js
@@ -0,0 +1,8 @@
+function get_greetings(){
+return ['Hello', 'World', 'Park MyeongHyun'];
+}
+
+var greetings = get_greetings();
+document.write(greetings[0]);
+document.write(greetings[1]);
+document.write(greetings[2]);
