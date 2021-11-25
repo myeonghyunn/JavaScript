5a0b61f72aa17a3cdda0c8e576a07c52d9ae730d
Myeong Hyun Park <qwef106@gmail.com>
2021-11-25 19:33:39 +0900

    Add files via upload

---
+++ JavaScript-6-조건문/login_test.js
@@ -0,0 +1,6 @@
+var id = prompt("Type your ID : ");
+      var password = prompt("Type your Password : ");
+      if (id === "myeonghyun" && password == 1111) {
+        alert("You're now safely logged in!"); 
+      } else {
+        alert("Login failed!");
